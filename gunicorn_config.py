fde09d4baed1a9db0c0d16d4cfe3cb7af8e7691e
git-dariel <104293978+git-dariel@users.noreply.github.com>
2025-02-17 04:13:41 +0800

    feat: update again for memory usage

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,7 +1,12 @@
 bind = "0.0.0.0:10000"
 workers = 1
-threads = 2
-timeout = 120
+threads = 1
+timeout = 300
 worker_class = "gthread"
 max_requests = 10
-max_requests_jitter = 3 
+max_requests_jitter = 3
+worker_tmp_dir = "/dev/shm"
+preload_app = True
+# Reduce memory usage
+worker_connections = 100
+keepalive = 5 
